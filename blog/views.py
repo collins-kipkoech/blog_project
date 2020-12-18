b7e69d92551ee39e0a2bdc981cc5af2d14b28d79
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-18 07:33:04 +0300

    created post model

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
-
+posts = [
+    {
+        'title':'new post',
+        'author':'collins',
+        'content':'new post content',
+    },
+    {
+        'title':'new post 1',
+        'author':'collins',
+        'content':'new post content two',
+    }
+]
 # Create your views here.
 def home_view(request):
-    return render(request,'blog/home.html')
+    context = {'posts':posts}
+    return render(request,'blog/home.html',context)
    
