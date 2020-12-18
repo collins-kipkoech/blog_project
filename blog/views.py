cb4cf23c8ed7f390c9262e9c96dbc107815c8b9d
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-18 10:49:25 +0300

    created a view function to handle user registration

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-posts = [
-    {
-        'title':'new post',
-        'author':'collins',
-        'content':'new post content',
-    },
-    {
-        'title':'new post 1',
-        'author':'collins',
-        'content':'new post content two',
-    }
-]
+
 # Create your views here.
 def home_view(request):
+    posts = Post.objects.all()
     context = {'posts':posts}
     return render(request,'blog/home.html',context)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Your account has been created successfully')
+            return redirect('home-view')
+    else:
+        form = UserCreationForm()
+        messages.warning(request,'Enter details')
+    context = {
+        'form':form,
+    }
+    return render(request,'blog/register.html',context)
    
